0d546ffab44957eebe0953aaa60cecb6f78bfa32
Tirath957 <ktirath001@gmail.com>
2026-03-05 12:42:31 +0530

    Added Two Sum Fraud Detector

--- src/twosum/TwoSumFraudDetector.java
+++ src/twosum/TwoSumFraudDetector.java
@@ -1,4 +1,24 @@
 package twosum;
 
+import java.util.*;
+
 public class TwoSumFraudDetector {
+
+    public boolean findTwoSum(int[] arr,int target){
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num:arr){
+
+            if(set.contains(target-num))
+                return true;
+
+            set.add(num);
+
+        }
+
+        return false;
+
+    }
+
 }
