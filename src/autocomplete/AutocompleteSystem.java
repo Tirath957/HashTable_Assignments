5b7d0a5e4b2780b108f2f5118fc71032f5b7c250
Tirath957 <ktirath001@gmail.com>
2026-03-05 12:41:33 +0530

    Added Autocomplete System

--- src/autocomplete/AutocompleteSystem.java
+++ src/autocomplete/AutocompleteSystem.java
@@ -1,4 +1,30 @@
 package autocomplete;
 
+import java.util.*;
+
 public class AutocompleteSystem {
+
+    private HashMap<String,Integer> queries = new HashMap<>();
+
+    public void addQuery(String query){
+
+        queries.put(query,queries.getOrDefault(query,0)+1);
+
+    }
+
+    public List<String> search(String prefix){
+
+        List<String> results = new ArrayList<>();
+
+        for(String q:queries.keySet()){
+
+            if(q.startsWith(prefix))
+                results.add(q);
+
+        }
+
+        return results;
+
+    }
+
 }
