f51bfc67648b5e39fa71dad361faf7c8c6fcc4ad
Tirath957 <ktirath001@gmail.com>
2026-03-05 12:39:20 +0530

    Added DNS Cache system

--- src/dnscache/DNSCache.java
+++ src/dnscache/DNSCache.java
@@ -1,4 +1,43 @@
 package dnscache;
 
+import java.util.*;
+
+class DNSEntry{
+
+    String ip;
+    long expiryTime;
+
+    DNSEntry(String ip,long ttl){
+        this.ip = ip;
+        this.expiryTime = System.currentTimeMillis()+ttl;
+    }
+
+}
+
 public class DNSCache {
+
+    private HashMap<String,DNSEntry> cache = new HashMap<>();
+
+    public void addEntry(String domain,String ip,long ttl){
+
+        cache.put(domain,new DNSEntry(ip,ttl));
+
+    }
+
+    public String resolve(String domain){
+
+        DNSEntry entry = cache.get(domain);
+
+        if(entry==null)
+            return "Cache miss";
+
+        if(System.currentTimeMillis()>entry.expiryTime){
+            cache.remove(domain);
+            return "Cache expired";
+        }
+
+        return entry.ip;
+
+    }
+
 }
