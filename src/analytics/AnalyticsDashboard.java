be64f461cc8d7f43195f579eaac632f1ab13a7dd
Tirath957 <ktirath001@gmail.com>
2026-03-05 12:40:30 +0530

    Added Analytics Dashboard

--- src/analytics/AnalyticsDashboard.java
+++ src/analytics/AnalyticsDashboard.java
@@ -1,4 +1,19 @@
 package analytics;
 
+import java.util.*;
+
 public class AnalyticsDashboard {
+
+    private HashMap<String,Integer> pageViews = new HashMap<>();
+    private HashMap<String,Set<String>> uniqueVisitors = new HashMap<>();
+
+    public void processEvent(String url,String userId){
+
+        pageViews.put(url,pageViews.getOrDefault(url,0)+1);
+
+        uniqueVisitors.putIfAbsent(url,new HashSet<>());
+        uniqueVisitors.get(url).add(userId);
+
+    }
+
 }
