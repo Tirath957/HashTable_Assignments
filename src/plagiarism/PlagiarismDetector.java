9fad77b15873ce2c20f6f5c194e5f6a5e18e7dbe
Tirath957 <ktirath001@gmail.com>
2026-03-05 12:39:54 +0530

    Added Plagiarism Detection System

--- src/plagiarism/PlagiarismDetector.java
+++ src/plagiarism/PlagiarismDetector.java
@@ -1,4 +1,24 @@
 package plagiarism;
 
+import java.util.*;
+
 public class PlagiarismDetector {
+
+    private HashMap<String,Set<String>> ngramIndex = new HashMap<>();
+
+    public void indexDocument(String docId,String text){
+
+        String[] words = text.split(" ");
+
+        for(int i=0;i<words.length-4;i++){
+
+            String gram = words[i]+" "+words[i+1]+" "+words[i+2]+" "+words[i+3]+" "+words[i+4];
+
+            ngramIndex.putIfAbsent(gram,new HashSet<>());
+            ngramIndex.get(gram).add(docId);
+
+        }
+
+    }
+
 }
