1a655d8e5d8aac6d990dbb2b750783399a111453
Tirath957 <ktirath001@gmail.com>
2026-03-05 12:43:12 +0530

    Added Multi Level Cache System

--- src/multilevelcache/MultiLevelCacheSystem.java
+++ src/multilevelcache/MultiLevelCacheSystem.java
@@ -1,4 +1,29 @@
 package multilevelcache;
 
-public class MultiLevelCacheSystem 1{
+import java.util.*;
+
+public class MultiLevelCacheSystem {
+
+    private HashMap<String,String> L1 = new HashMap<>();
+    private HashMap<String,String> L2 = new HashMap<>();
+
+    public String get(String key){
+
+        if(L1.containsKey(key))
+            return L1.get(key);
+
+        if(L2.containsKey(key)){
+
+            String value = L2.get(key);
+
+            L1.put(key,value);
+
+            return value;
+
+        }
+
+        return "Cache miss";
+
+    }
+
 }
