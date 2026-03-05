6e14ecc2e8b31357ca4382a3ee60cba5c0522130
Tirath957 <ktirath001@gmail.com>
2026-03-05 12:42:03 +0530

    Added Parking Lot Manager

--- src/parking/ParkingLotManager.java
+++ src/parking/ParkingLotManager.java
@@ -1,4 +1,21 @@
 package parking;
 
+import java.util.*;
+
 public class ParkingLotManager {
+
+    private HashMap<String,Integer> parking = new HashMap<>();
+
+    public void parkVehicle(String plate,int spot){
+
+        parking.put(plate,spot);
+
+    }
+
+    public void exitVehicle(String plate){
+
+        parking.remove(plate);
+
+    }
+
 }
